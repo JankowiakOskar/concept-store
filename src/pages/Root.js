846f1473e7d646983d062c3e440f43d47c236c36
oskarjankowiak <jankowiak31@gmail.com>
2020-11-17 22:15:46 +0100

    init files

---
+++ src/pages/Root.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ReactComponent as Logo } from '../assets/svgs/Logo.svg'
+
+const Root = () => {
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  )
+}
+
+export default Root
